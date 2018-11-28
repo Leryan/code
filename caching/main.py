10c4ec3df4a2d23bcefb1c65515bded0a05cb5b6
Florent Peterschmitt <florent@peterschmitt.fr>
2018-11-28 22:02:30 +0100

    .

--- caching/main.py
+++ caching/main.py
@@ -1,8 +1,8 @@
-from caching.backend import Backend
 from caching.serializers import Serializer
 from caching.cache import Cache
 
 from app import Controller
+from backend import Backend
 
 if __name__ == '__main__':
     c = Controller(Backend({}, Cache(Serializer())))
