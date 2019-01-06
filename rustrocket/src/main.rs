26288d091aec340982cb4d6a0afef212c750e2b3
Florent Peterschmitt <florent@peterschmitt.fr>
2019-01-06 15:39:27 +0100

    it's a revolution

--- rustrocket/src/main.rs
+++ rustrocket/src/main.rs
@@ -1,9 +1,22 @@
 extern crate clap;
+extern crate cursive;
 extern crate reqwest;
 extern crate rocket;
 
 use clap::{App, Arg};
 
+use cursive::direction::Orientation;
+use cursive::event::Event;
+use cursive::event::Key;
+use cursive::menu::MenuTree;
+use cursive::view::ScrollStrategy;
+use cursive::views::BoxView;
+use cursive::views::EditView;
+use cursive::views::IdView;
+use cursive::views::LinearLayout;
+use cursive::views::ScrollView;
+use cursive::views::TextView;
+use cursive::Cursive;
 use rocket::chat::Chat;
 
 fn main() {
@@ -34,6 +47,42 @@ fn main() {
         args.value_of("user").unwrap(),
         args.value_of("password").unwrap(),
     );
-    println!("{:?}", client.login());
-    println!("{:?}", client.channels_list());
+
+    let mut siv = Cursive::default();
+
+    siv.menubar().add_subtree(
+        "Paper - Esc to access menu",
+        MenuTree::new().leaf("Quit", |s| s.quit()),
+    );
+    siv.set_autohide_menu(false);
+    siv.add_global_callback(Key::Esc, |s| s.select_menubar());
+
+    siv.add_global_callback(Event::CtrlChar('l'), |s| {
+        s.call_on_id("input", |view: &mut EditView| {
+            view.set_content("");
+        });
+    });
+    let tchatview = ScrollView::new(IdView::new("tchat", TextView::empty()))
+        .scroll_strategy(ScrollStrategy::StickToBottom);
+    let inputview = IdView::new("input", EditView::new().on_submit(input_text));
+    siv.add_fullscreen_layer(BoxView::with_full_screen(
+        LinearLayout::new(Orientation::Vertical)
+            .child(tchatview)
+            .child(inputview),
+    ));
+
+    siv.run();
+}
+
+fn input_text(s: &mut Cursive, content: &str) {
+    s.call_on_id("tchat", |view: &mut TextView| match content {
+        "" => {}
+        _ => {
+            view.append(content);
+            view.append("\n");
+        }
+    });
+    s.call_on_id("input", |view: &mut EditView| {
+        view.set_content("");
+    });
 }
