642954a6f7a2722976e506485815fa02fb5428f2
Florent Peterschmitt <florent.peterschmitt@livingpackets.com>
2020-01-03 01:31:35 +0100

    (づ ￣ ³￣)づ

--- misc/data-oriented-design/soa.c
+++ misc/data-oriented-design/soa.c
@@ -27,29 +27,44 @@ typedef struct SOA {
     int64_t *z;
 } SOA;
 
+void soa_translate_fetchone(SOA soa, Vec3D vec) {
+#define SOA_o(field) soa.field[i] += vec.field;
+    uint64_t max = soa.s;
+
+    for (uint64_t i = 0; i < max; ++i) {
+        SOA_o(x)
+    }
+    for (uint64_t i = 0; i < max; ++i) {
+        SOA_o(y)
+    }
+    for (uint64_t i = 0; i < max; ++i) {
+        SOA_o(z)
+    }
+}
+
 void soa_translate(SOA soa, Vec3D vec) {
 #define INC 4
 #define INCM INC-1
-#define SOA(field, offset) soa.field[i+offset] += vec.field;
+#define SOA_m(field, offset) soa.field[i+offset] += vec.field;
     uint64_t max = soa.s + INCM; // 100% guaranteed *with* buffer overflow.
 
     for (uint64_t i = 0; i < max; i += INC) {
-        SOA(x, 0)
-        SOA(x, 1)
-        SOA(x, 2)
-        SOA(x, 3)
+        SOA_m(x, 0)
+        SOA_m(x, 1)
+        SOA_m(x, 2)
+        SOA_m(x, 3)
     }
     for (uint64_t i = 0; i < max; i += INC) {
-        SOA(y, 0)
-        SOA(y, 1)
-        SOA(y, 2)
-        SOA(y, 3)
+        SOA_m(y, 0)
+        SOA_m(y, 1)
+        SOA_m(y, 2)
+        SOA_m(y, 3)
     }
     for (uint64_t i = 0; i < max; i += INC) {
-        SOA(z, 0)
-        SOA(z, 1)
-        SOA(z, 2)
-        SOA(z, 3)
+        SOA_m(z, 0)
+        SOA_m(z, 1)
+        SOA_m(z, 2)
+        SOA_m(z, 3)
     }
 }
 
@@ -63,16 +78,22 @@ int main(void) {
     SOA soa = {ELEMS, soa_x, soa_y, soa_z};
     Vec3D translate = {-1, 2, 8};
 
-    double times = 0.0;
+    double times_m = 0.0;
+    double times_s = 0.0;
     int runs = 0;
 
     for (runs = 0; runs < 5; runs++) {
         for (int i = 0; i < LOOPS; i++) {
             double ts = get_time();
+            soa_translate_fetchone(soa, translate);
+            times_s += (double)(get_time() - ts) / CLOCKS_PER_SEC;
+            //
+            ts = get_time();
             soa_translate(soa, translate);
-            times += (double)(get_time() - ts) / CLOCKS_PER_SEC;
+            times_m += (double)(get_time() - ts) / CLOCKS_PER_SEC;
         }
         printf("loop %d: %ld %ld %ld\n", runs, soa_x[10], soa_y[10], soa_z[10]);
     }
-    printf("%f\n", times / (double)(LOOPS * runs));
+    printf("fetch one  : %f\n", times_s / (double)(LOOPS * runs));
+    printf("fetch multi: %f\n", times_m / (double)(LOOPS * runs));
 }
